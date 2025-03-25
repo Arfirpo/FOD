3edb8ca485af04e775084c420388aa80a6d72de5
Agustín Firpo <agustin_firpo@outlook.com.ar>
2025-03-24 21:21:24 -0300

    ejercicio 1 completo

--- practica-1/ejercicio-1.pas
+++ practica-1/ejercicio-1.pas
@@ -1,5 +1,31 @@
 
+Program ejercicio1;
 
-//Realizar un algoritmo que cree un archivo de números enteros no ordenados y permita incorporar datos al archivo. Los números son ingresados desde teclado. La carga finaliza cuando se ingresa el número 30000, que no debe incorporarse al archivo. El nombre del archivo debe ser proporcionado por el usuario desde teclado.
+Type 
+  archivo = file Of integer;
 
+  Procedure agregarNumeros(Var arc: archivo);
+  Var 
+    nro: integer;
+  Begin
+    writeln('Ingrese un numero de teclado');
+    readln(nro);
+    While (nro <> 30000) Do
+      Begin
+        write(arc, nro);
+        writeln('Ingrese un numero de teclado');
+        readln(nro);
+      End;
+    close(arc);
+  End;
 
+Var 
+  nombre: string[15];
+  arc: archivo;
+Begin
+  writeln('Ingrese un nombre de archivo');
+  readln(nombre);
+  assign(arc, nombre);
+  rewrite(arc);
+  agregarNumeros(arc);
+End.
