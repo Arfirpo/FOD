d1681b7ebe6102764b363f9311a82c7cc219e722
Agustin Firpo <agustin_@outlook.com.ar>
2025-03-30 22:05:06 -0300

    agregue funcion para chequear que el numero de empleado no se repita

--- practica-1/ejercicio-4.pas
+++ practica-1/ejercicio-4.pas
@@ -5,7 +5,7 @@ c. Exportar el contenido del archivo a un archivo de texto llamado “todos_empl
 d. Exportar a un archivo de texto llamado: “faltaDNIEmpleado.txt”, los empleados que no tengan cargado el DNI (DNI en 00).
 NOTA: Las búsquedas deben realizarse por número de empleado.}
 
-Program ejercicio3;
+Program ejercicio4;
 
 const corte = 'fin';
 
@@ -24,22 +24,23 @@ Type
 
 {modulos}
 
+
+procedure leerEmpleado(var e: Empleado);
+begin
+  with e do begin
+    write('Ingrese el apellido del empleado: '); readln(apellido);
+    if(apellido <> corte) then begin
+      write('Ingrese el nombre del empleado: '); readln(nombre);
+      write('Ingrese el Nro del empleado: '); readln(nro);
+      write('Ingrese el dni del empleado: '); readln(dni);
+      write('Ingrese la edad del empleado: '); readln(edad);
+    end;
+  end;
+end;
+
 procedure crearArchivoEmpleados();
 
   procedure cargarArchivoEmpleados(var Empleados: ArchivoEmpleados);
-  
-    procedure leerEmpleado(var e: Empleado);
-    begin
-      with e do begin
-        write('Ingrese el apellido del empleado: '); readln(apellido);
-        if(apellido <> corte) then begin
-          write('Ingrese el nombre del empleado: '); readln(nombre);
-          write('Ingrese el Nro del empleado: '); readln(nro);
-          write('Ingrese el dni del empleado: '); readln(dni);
-          write('Ingrese la edad del empleado: '); readln(edad);
-        end;
-      end;
-    end;
   
   var
     e: Empleado;
@@ -143,6 +144,40 @@ procedure procesarArchivoEmpleados();
     if not alMenosUno then writeln('No existen empleados proximos a la edad jubilatoria.')
   end;
 
+procedure agregarEmpleados(var Empleados: ArchivoEmpleados);
+
+  function numeroLibre(var Empleados: ArchivoEmpleados; num: integer): boolean;
+  var 
+    e: Empleado; 
+    ok: Boolean;
+  begin
+    Reset(Empleados); // Reset to beginning of file
+    ok := true;
+    while not(Eof(Empleados)) and ok do begin
+      Read(Empleados,e);
+      if(e.nro = num) then ok := false;
+    end;
+    numeroLibre := ok;
+  end;
+
+var 
+  e: Empleado;
+  filePos: integer; // To store the original file position
+begin
+  Reset(Empleados);
+  Seek(Empleados, FileSize(Empleados)); // Position at end of file
+  leerEmpleado(e);
+  while e.apellido <> corte do begin
+    if numeroLibre(Empleados, e.nro) then begin
+      Seek(Empleados, FileSize(Empleados)); // Reset position to end of file
+      Write(Empleados, e);
+    end
+    else
+      writeln('Error: Número de empleado ya existe');
+    leerEmpleado(e);
+  end;
+end;
+
 var
   nombre: string[10];
   Empleados: ArchivoEmpleados;
@@ -162,18 +197,19 @@ begin
     writeln();
     writeln('1.- Buscar empleado por nombre o apellido.');
     writeln('2.- Ver lista de empleados completa.');
-    writeln('3.- Lista de empleados proximos a jubilarse.');
+    writeln('3.- Ver lista de empleados proximos a jubilarse.');
+    writeln('4.- Agregar empleados al archivo.');
     writeln('0.- Salir.');
     writeln();
     repeat
       write('Opcion elegida: '); readln(opc);
       writeln();
-      if (opc < 0) or (opc > 3) then
+      if (opc < 0) or (opc > 4) then
       begin
         writeln('Opcion incorrecta. Intente nuevamente. -0 para salir-');
         writeln();
       end;
-    until (opc = 0) or (opc = 1) or (opc = 2) or (opc = 3);
+    until (opc >= 0) and (opc <= 4);
     case opc of
       1:  begin
             write('Inrese Nombre/Apellido del Empleado buscado: '); readln(nomOape);
@@ -181,6 +217,7 @@ begin
           end;
       2: listaDeEmpleados(Empleados);
       3: listaJubilacion(Empleados);
+      4: agregarEmpleados(Empleados);
       0: break;
     end;
   until false;
