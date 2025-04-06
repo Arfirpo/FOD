b9eda093aa6b4544955fa98ff9a4f596603a4eca
Agustin Firpo <agustin_@outlook.com.ar>
2025-04-06 02:54:22 -0300

    ejercicio 7 terminado. Practica 1 completa.

--- practica-1/ejercicio-7.pas
+++ practica-1/ejercicio-7.pas
@@ -5,19 +5,33 @@ NOTA: El nombre del archivo binario es proporcionado por el usuario desde el tec
 
 program ejercicio_7_P1;
 
+  const corte = 'fin';
+
 { Sección de tipos }
 type
-  str15 = string[15];
+  str30 = string[30];
   Novela = record
     cod: integer;
-    nom: str15;
-    gen: str15;
+    nom: str30;
+    gen: str30;
     precio: real;
   end;
 
   ArchivoNovelas = file of Novela;
 
 { Modulos }
+
+procedure leerNovela(var n: Novela);
+begin
+  with n do begin
+    Write('Ingrese nombre de la novela: '); ReadLn(nom);
+    if nom <> corte then begin
+      Write('Ingrese codigo de la novela: '); ReadLn(cod);
+      Write('Ingrese precio de la novela: '); ReadLn(precio);
+      Write('Ingrese genero de la novela: '); ReadLn(gen);      
+    end;
+  end;
+end;
 
 function buscarNovela(var Novelas: ArchivoNovelas; cod: integer): integer;
 var pos: integer; n: Novela; ok: Boolean;
@@ -53,10 +67,133 @@ begin
   Close(Novelas); Close(carga);
 end;
 
-// procedure actualizarArchivo(var Novelas: ArchivoNovelas);
-// var
-// begin
-// end;
+procedure actualizarArchivo(var Novelas: ArchivoNovelas);
+
+  procedure agregarNovela(var Novelas: ArchivoNovelas);
+  var n: Novela;
+  begin
+    Reset(Novelas);
+    Seek(Novelas,FileSize(Novelas));
+    leerNovela(n);
+    while n.nom <> corte do begin
+      Write(Novelas,n);
+      leerNovela(n);
+    end;
+    WriteLn('Novelas agregadas con exito.');
+    WriteLn();
+    Close(Novelas);
+  end;
+
+  procedure modificarNovela(var Novelas: ArchivoNovelas);
+  var opc: integer; cod: Integer; pos: integer; n: Novela;
+  begin
+    Writeln();
+    Write('Ingrese codigo de la novela a buscar: '); ReadLn(cod);
+    Writeln();
+    pos := buscarNovela(Novelas,cod);
+    if pos <> -1 then begin
+      Reset(Novelas);
+      Seek(Novelas,pos);
+      Read(Novelas,n);
+      repeat
+        Writeln('------ Menu de Modificacion ------');
+        Writeln();
+        Writeln('----> Opciones: ');
+        Writeln();
+        Writeln('1.----> Modificar nombre de la novela.');
+        Writeln('2.----> Modificar genero de la novela.');
+        Writeln('3.----> Modificar precio de la novela.');
+        Writeln('4.----> Modificar codigo de la novela.');
+        Writeln();
+        Write('Ingrese la opcion elegida -0 para finalizar-: '); Readln(opc);
+        Writeln();
+        case opc of
+          1: begin Write('Ingrese el nombre de la novela: '); ReadLn(n.nom); end;
+          2: begin Write('Ingrese el genero de la novela: '); ReadLn(n.gen); end;
+          3: begin Write('Ingrese el precio de la novela: '); ReadLn(n.precio); end;
+          4: begin Write('Ingrese el codigo de la novela: '); ReadLn(n.cod); end;
+        end;
+        Write(Novelas,n);
+        WriteLn();
+        WriteLn('Novela modificada con exito.');
+        WriteLn();
+      until(opc = 0);      
+    end
+    else begin
+      WriteLn('No se encontro la novela buscada.');
+      WriteLn();
+    end;    
+  end;
+
+var opc: integer;
+begin
+  repeat
+    Writeln();
+    Writeln('------ Menu de Actualizacion ------');
+    Writeln();
+    Writeln('----> Opciones: ');
+    Writeln();
+    Writeln('1.----> Agregar novelas al archivo.');
+    Writeln('2.----> Modificar una novela ya existente.');
+    Writeln();
+    Write('Ingrese la opcion elegida -0 para finalizar-: '); Readln(opc);
+    Writeln();
+    case opc of
+      1: agregarNovela(Novelas);
+      2: modificarNovela(Novelas);
+    end;
+  until(opc = 0);
+end;
+
+procedure listarArchivo(var Novelas: ArchivoNovelas);
+
+  procedure imprimirNovela(n: Novela);
+  begin
+    with n do begin
+      WriteLn('Titulo: ',nom);
+      WriteLn('Codigo: ',cod);
+      WriteLn('Precio: $',precio:0:2);
+      WriteLn('Genero: ',gen);
+    end;
+  end;
+
+var n: Novela;
+begin
+  Reset(Novelas);
+  WriteLn('Lista de Novelas');
+  WriteLn('================');
+  WriteLn();
+  while not(Eof(Novelas)) do begin
+    Read(Novelas,n);
+    imprimirNovela(n);
+    Writeln();
+  end;
+  Close(Novelas);
+  WriteLn();
+  WriteLn('================');
+end;
+
+procedure exportarArchivo(var Novelas: ArchivoNovelas);
+var carga: Text; n: Novela; nomArch: String;
+begin
+  Reset(Novelas);
+  Write('Ingrese nombre del archivo exportado: '); ReadLn(nomArch);
+  Assign(carga,nomArch + '.txt');
+  Rewrite(carga);
+  WriteLn(carga,'Lista de Novelas');
+  WriteLn(carga,'================');
+  WriteLn(carga);
+  while not(Eof(Novelas)) do begin
+    Read(Novelas,n);
+    WriteLn(carga,'Codigo: ',n.cod,' - ','Titulo: ',n.nom,' - ','Precio: $',n.precio:0:2,' - ','Genero: ',n.gen,'.');
+  end;
+  WriteLn(carga);
+  WriteLn(carga,'================');
+  WriteLn();
+  WriteLn('Archivo exportado exitosamente.');
+  WriteLn();
+  Close(Novelas); Close(carga);
+end;
 
 { Programa Principal}
 var novelas: ArchivoNovelas; opc: integer; nomArch: string;
@@ -69,17 +206,21 @@ begin
     Writeln();
     Writeln('1.----> Crear archivo de novelas.');
     Writeln('2.----> Actualizar archivo de novelas.');
+    Writeln('3.----> Mostrar archivo de novelas.');
+    Writeln('4.----> Exportar archivo de novelas.');
     Writeln();
     Write('Ingrese la opcion elegida -0 para finalizar-: '); Readln(opc);
     Writeln();
-    if(opc >= 1) and (opc <= 2) then begin
+    if(opc >= 1) and (opc <= 4) then begin
       Write('Ingrese el nombre del achivo a crear/leer: '); Readln(nomArch);
       Assign(Novelas,nomArch + '.bin');
       Writeln();
     end;
     case opc of
       1: crearArchivo(Novelas);
-      // 2: actualizarArchivo(Novelas);
+      2: actualizarArchivo(Novelas);
+      3: listarArchivo(Novelas);
+      4: exportarArchivo(Novelas);
     end;
   until(opc = 0);
 end.
