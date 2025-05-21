3e198f4427a44de1b1772a92b46fba071f3e8e23
agustin firpo <agustin_firpoutlook.com.ar>
2025-05-21 14:28:32 -0300

    ejercicio 2 - practica 2 - en proceso

---
+++ resoluciones/helpers/crearDetalle.pas
@@ -0,0 +1,76 @@
+program crearDetalle;
+
+type
+	str10 = string[10];
+
+  comision = record
+    cod_empleado: integer;
+    nom: str10;
+    comision: real;
+  end;
+
+	detalle = file of comision;
+
+	lCom = ^nCom;
+  nCom = record
+    dato: comision;
+    sig: lCom;
+  end;
+
+{modulos}
+  procedure leerComision(var c: comision);
+  begin
+    with c do begin
+      write('Ingrese codigo de empleado: '); readln(cod_empleado);
+      if (cod_empleado <> -1) then begin  // Changed "do" to "then"
+        write('Ingrese nombre del empleado: '); readln(nom);
+        write('Ingrese monto de la comision del empleado: '); readln(comision);
+      end;
+    end;
+  end;
+
+  procedure insertarOrdenado(var l: lCom; c: comision);
+  var
+    nue, ant, act: lCom;
+  begin
+    new(nue);
+    nue^.dato := c;
+    nue^.sig := nil;
+    ant := nil;
+    act := l;
+    while (act <> nil) and (act^.dato.cod_empleado < c.cod_empleado) do begin
+      ant := act;
+      act := act^.sig;
+    end;
+    if (ant = nil) then
+      l := nue
+    else
+      ant^.sig := nue;
+    nue^.sig := act;
+  end;
+
+  procedure cargarArchivoDetalle(var det: detalle; l: lCom);
+  begin
+    Rewrite(det);
+    while (l <> nil) do begin
+      write(det, l^.dato);
+      l := l^.sig;
+    end;
+    Close(det);
+  end;
+{Programa Principal}
+var
+	det1: detalle;
+  c: comision;
+  pri: lCom;
+begin
+	Assign(det1,'detalle');
+  pri := nil;
+  leerComision(c);
+  while (c.cod_empleado <> -1) do begin
+    insertarOrdenado(pri, c);
+    leerComision(c);
+  end;
+  cargarArchivoDetalle(det, pri);
+end.
+
--- resoluciones/practica-2/ejercicio-1.pas
+++ resoluciones/practica-2/ejercicio-1.pas
@@ -15,70 +15,7 @@ type
 
   maestro = file of empleado;
   detalle = file of comision;
-  
-  // Added missing type definition
-  lCom = ^nCom;
-  nCom = record
-    dato: comision;
-    sig: lCom;
-  end;
 
-{modulos}
-procedure crearArchivoDetalle(var det: detalle);
-
-  procedure leerComision(var c: comision);
-  begin
-    with c do begin
-      write('Ingrese codigo de empleado: '); readln(cod_empleado);
-      if (cod_empleado <> -1) then begin  // Changed "do" to "then"
-        write('Ingrese nombre del empleado: '); readln(nom);
-        write('Ingrese monto de la comision del empleado: '); readln(comision);
-      end;
-    end;
-  end;
-
-  procedure insertarOrdenado(var l: lCom; c: comision);
-  var
-    nue, ant, act: lCom;
-  begin
-    new(nue);
-    nue^.dato := c;
-    nue^.sig := nil;
-    ant := nil;
-    act := l;
-    while (act <> nil) and (act^.dato.cod_empleado < c.cod_empleado) do begin
-      ant := act;
-      act := act^.sig;
-    end;
-    if (ant = nil) then
-      l := nue
-    else
-      ant^.sig := nue;
-    nue^.sig := act;
-  end;
-
-  procedure cargarArchivoDetalle(var det: detalle; l: lCom);
-  begin
-    Rewrite(det);
-    while (l <> nil) do begin
-      write(det, l^.dato);
-      l := l^.sig;
-    end;
-    Close(det);
-  end;
-
-var 
-  c: comision;
-  pri: lCom;
-begin
-  pri := nil;
-  leerComision(c);
-  while (c.cod_empleado <> -1) do begin
-    insertarOrdenado(pri, c);
-    leerComision(c);
-  end;
-  cargarArchivoDetalle(det, pri);
-end;
 
 procedure crearArchivoMaestro(var det1: detalle; var mae1: maestro);
 var
@@ -88,9 +25,9 @@ var
   codAct: integer;
 begin
   Reset(det1);
-  Rewrite(mae1);  // Changed maestro to mae1
+  Rewrite(mae1); 
   
-  if not Eof(det1) then begin  // Added check before first read
+  if not Eof(det1) then begin  
     read(det1, regD);
     
     while not Eof(det1) do begin
@@ -119,6 +56,5 @@ var
 begin
   Assign(mae1, 'maestro');
   Assign(det1, 'detalle');
-  crearArchivoDetalle(det1);
   crearArchivoMaestro(det1, mae1);
 end.
