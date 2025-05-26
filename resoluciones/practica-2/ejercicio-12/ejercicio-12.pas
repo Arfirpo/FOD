c963446ec182f437bac83f3f7d24367ab3390f47
agustin firpo <agustin_firpoutlook.com.ar>
2025-05-26 13:25:40 -0300

    ejercicio 12 - practica 2 - en proceso

---
+++ resoluciones/practica-2/ejercicio-12/ejercicio-12.pas
@@ -0,0 +1,58 @@
+program ejercicio_12_P2;
+
+const
+	valorAlto = 9999;
+
+
+type
+	
+	Fecha = record
+		dia: 1..31;
+		mes: 1..12;
+		anio: integer;
+	end;
+
+	Acceso = record
+		fecha: Fecha;
+		idUsuario: integer;
+		tiempoAcceso: real;
+	end;
+
+	ArchivoMaestro = file of Acceso;
+
+{modulos}
+procedure leerMaestro(var maestro: ArchivoMaestro; var dato: Acceso);
+begin
+	if(not(Eof(maestro))) then
+		read(maestro,dato)
+	else
+		dato.fecha.anio := valorAlto;
+end;
+
+procedure generarReporte(var maestro: ArchivoMaestro; anio: integer);
+var
+	RegM: Acceso;
+	mesAct,diaAct,usuarioAct: integer;
+	totAnio,totMes,totDia,totUsuario: real;
+begin
+	Reset(maestro);
+	leerMaestro(maestro,regM);
+	while (regM.fecha.anio <> valorAlto) and (regM.fecha.anio <> anio) do
+		leerMaestro(maestro,regM);
+
+	if not(regM.fecha.anio <> valorAlto) and not(regM.fecha.anio = anio) then
+		writeln('El anio no fue encontrado.');
+	else
+
+end;
+
+
+{Programa principal}
+var
+	maestro: ArchivoMaestro;
+	anio: integer;
+begin
+	Assign(maestro,'maestro.bin');
+	write('Ingrese el anio a buscar: '); readln(anio);
+	generarReporte(maestro,anio);
+end.
