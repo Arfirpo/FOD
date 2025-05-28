9c8eaddd61dd0cb4863be3eb8346b12a5936a4d9
agustin firpo <agustin_firpoutlook.com.ar>
2025-05-28 15:21:30 -0300

    ejercicio 17 - practica 2 - completo

--- resoluciones/practica-2/ejercicio-17/ejercicio-17.pas
+++ resoluciones/practica-2/ejercicio-17/ejercicio-17.pas
@@ -20,17 +20,87 @@ type
     fechaVenta: str10;
   end;
 
-  ArchivoMaestro = file of Motos;
+  ArchivoMaestro = file of Moto;
   ArchivoDetalle = file of Venta;
 
-  VectorDeArchivosDetalle = array [1..10] of ArchivoDetalle;
-  VectorDeRegistrosDetalle = array [1..10] of Venta;
+  VectorArchivosDetalle = array [1..10] of ArchivoDetalle;
+  VectorRegistrosDetalle = array [1..10] of Venta;
 
 { Modulos }
 
-procedure leerDetalle();
-procedure minimo();
-procedure actualizarMaestro();
+procedure leerDetalle(var detalle: ArchivoDetalle; var dato: Venta);
+begin
+  if(not(Eof(detalle))) then
+    read(detalle,venta)
+  else
+    dato.codMoto := valorAlto;
+end;
+
+
+procedure minimo(var vD: VectorArchivoDetalle; var vR: VectorRegistroDetalle; var minregD: Venta);
+var
+  i,pos: integer;
+begin
+  minRegD.codMoto := valorAlto;
+  for i := 1 to dimF do begin
+    if(vR[i].codMoto < minRegD.codMoto) then begin
+      minRegD := vR[i];
+      pos := i;
+    end;
+  end;
+  if(minregD.codMoto <> valorAlto) then
+    leerDetalle(vD[pos], vR[pos]);
+end;
+
+procedure actualizarMaximo(motoAct,cantVentas: integer; var maxMoto,maxVentas: integer);
+begin
+  if(cantVentas > maxVentas) then begin
+    maxVentas := cantVentas;
+    maxMoto := motoAct;
+  end;
+end;
+
+procedure actualizarMaestro(var maestro: ArchivoMaestro; var vD: VectorArchivosDetalle);
+var
+  i,cantVentas,maxVentas,motoAct,maxMoto:integer;
+  minRegD: Venta;
+  vR: VectorRegistrosDetalle;
+  regM: Moto;
+begin
+  Reset(maestro);
+  for i := 1 to dimF do begin
+    Reset(vD[i]);
+    leerDetalle(vD[I],vR[i]);
+  end;
+  minimo(vD,vR,minRegD);
+  read(maestro,regM);
+  maxMoto := -1;
+  maxVentas := -1;
+
+  while (minRegD.codMoto <> valorAlto) do begin
+    motoAct := minregD.codMoto;
+    
+    while (regM.codMoto <> motoAct) do
+      read(maestro,regM);
+    
+    cantVentas := 0;
+    while (minRegD.codMoto = motoAct) do begin
+      cantVentas := cantVentas + 1;
+      minimo(vD,vR,minRegD);
+    end;
+
+    actualizarMaximo(motoAct,cantVentas,maxMoto,maxVentas);
+
+    regM.stock := regM.stock - cantVentas;
+    seek(maestro,filepos(maestro) - 1);
+    write(maestro,regM);
+
+  end;
+  Writeln('La moto con codigo Nro ',maxMoto,' fue la mas vendida');
+  close(maestro);
+  for i := 1 to dimF do 
+    close(vD[i]);
+end;
 
 { Programa Principal}
 var
