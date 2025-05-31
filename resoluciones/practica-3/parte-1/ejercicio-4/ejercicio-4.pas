ffd0094b363f4fc8a85b1bdc596c299f6d059487
Agustin Firpo <agustin_@outlook.com.ar>
2025-05-31 18:10:33 -0300

    ejercicio 4 - practica 3 - en proceso.

--- resoluciones/practica-3/parte-1/ejercicio-4/ejercicio-4.pas
+++ resoluciones/practica-3/parte-1/ejercicio-4/ejercicio-4.pas
@@ -20,15 +20,51 @@ begin
     dato.codigo := valorAlto;
 end;
 
+procedure agregarFlor(var flores: tArchFlores; nombre: String; codigo: integer);
+var 
+  nuevo,cabecera,libre: reg_flor;
+  posLibre: integer;
+begin
+  Reset(flores);
+  nuevo.codigo := codigo;
+  nuevo.nombre := nombre;
+  leer(flores,cabecera);
+
+  if(cabecera.codigo < 0) then begin
+    posLibre := -cabecera.codigo;
+    Seek(flores,posLibre);
+    Read(flores,libre);
+    Seek(flores,posLibre);
+    Write(flores,nuevo);
+    Seek(flores,0);
+    Write(flores,libre);
+  end
+  else begin
+    Seek(flores,FileSize(flores));
+    Write(flores,nuevo);
+  end;
+  Close(flores);
+end;
+
 
 {Programa Principal}
 var
   flores: tArchFlores;
   nom: String;
+  nomFlor: strng[45];
+  cod: integer;
+  f: reg_flor;
 Begin
   Write('Ingrese el nombre del archivo a abrir: '); 
   Readln(nom);
   nom := nom + '.bin';
   Assign(flores,nom);
-  agregarFlor(flores);
+  f.codigo := 0; //codigo cabecera
+  write(flores,f); //guardo la cabecera
+  Write('Ingresar codigo de flor: '); Readln(cod);
+  while (cod <> -1) do begin
+    Write('Ingrese nombre de la flor: '); ReadLn(nomFlor);
+    agregarFlor(flores,cod,nomFlor);
+    Write('Ingresar codigo de flor: '); Readln(cod);
+  end;
 End.
