36d798a38cbf4e50a070dcaa20cabad27bc47349
Agustín Firpo <agustin_firpo@outlook.com.ar>
2025-06-02 21:15:57 -0300

    ejercicio 2 - practica 3 (Pte.2) - completo.

--- resoluciones/practica-3/parte-2/ejercicio-2/ejercicio-2.pas
+++ resoluciones/practica-3/parte-2/ejercicio-2/ejercicio-2.pas
@@ -12,11 +12,11 @@ type
 
 	ArchivoMaestro = file of MesaElectoral;
 
-	lMesas = ^nMesas;
+	lCodigos = ^nCodigos;
 
-	nMesas = record
+	nCodigos = record
 		dato: MesaElectoral;
-		sig: lMesas;
+		sig: lCodigos;
 	end;
 
 {Modulos}
@@ -28,13 +28,54 @@ begin
 		dato.codLoc := valorAlto;
 end;
 
-procedure agregarAdelante(var l: lMesas; m: MesaElectoral);
-var nue: lMesas;
+procedure agregarCodigoAdelante(var l: lCodigos; cod: integer);
+var nue: lCodigos;
 begin
 	new(nue);
-	nue^.dato := m;
+	nue^.dato := cod;
 	nue^.sig := l;
 	l := nue;
+end;
+
+procedure generaReporte(var maestro: ArchivoMaestro);
+var
+    regM: MesaElectoral;
+    pri: lCodigos;
+    codAct, totVotosMesa, totGen, posSig: integer;
+begin
+	Reset(maestro);
+	pri := nil;
+	leer(maestro, regM);
+	totGen := 0;
+	while (regM.cod <> valorAlto) do begin
+		if not buscarCodigo(pri, regM.codLoc) then begin
+			codAct := regM.codLoc;
+			agregarCodigoAdelante(pri, codAct);
+			posSig := FilePos(maestro);  // ← Posición del SIGUIENTE registro
+			
+			// Recorrer TODO el archivo para esta localidad
+			Reset(maestro);
+			leer(maestro, regM);
+			totVotosMesa := 0;
+			while (regM.cod <> valorAlto) do begin
+				if (regM.codLoc = codAct) then
+					totVotosMesa := totVotosMesa + regM.cantVotos;
+				leer(maestro, regM);
+			end;
+			
+			writeln('Localidad: ', codAct, ' - Total: ', totVotosMesa);
+			totGen := totGen + totVotosMesa;
+			
+			// Volver a la posición siguiente
+			Seek(maestro, posSig);
+			leer(maestro, regM);
+		end 
+		else
+			leer(maestro, regM);
+  end;
+    
+    writeln('Total general: ', totGen);
+    Close(maestro);
 end;
 
 {Programa Principal}
